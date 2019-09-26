d82b795d04f271f2b27f683ea79827de3b84c06b
lKingl <jinwenjie@live.com>
2019-09-27 00:25:19 +0800

    feat(account): add register and forget

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -5,66 +5,151 @@
         <img src="https://img.yzcdn.cn/vant/logo.png">
         <span>Vant</span>
       </h1>
-      <van-cell-group>
-        <h1>快捷登录注册</h1>
-        <van-field
-          v-model="username"
-          clearable
-          size="large"
-          placeholder="邮箱/手机号"
-          class="account-input"
-        />
 
+      <span class="account-span">快捷登录注册</span>
+
+      <van-field
+        v-model="username"
+        clearable
+        size="large"
+        placeholder="邮箱/手机号"
+        class="account-input"
+      >
+        <van-button v-if="status!=='login'" slot="button" size="small" type="primary">发送验证码</van-button>
+      </van-field>
+
+      <transition name="van-slide-up">
         <van-field
+          v-if="status!=='login'"
+          v-model="sms"
+          center
+          clearable
+          placeholder="请输入短信验证码"
+          class="account-input"
+        >
+        </van-field>
+      </transition>
+
+      <van-field
+        v-model="password"
+        type="password"
+        size="large"
+        placeholder="密码"
+        class="account-input"
+      />
+
+      <transition name="van-slide-up">
+        <van-field
+          v-if="status!=='login'"
           v-model="password"
           type="password"
           size="large"
-          placeholder="密码"
+          placeholder="确认密码"
           class="account-input"
         />
+      </transition>
 
-      </van-cell-group>
       <van-button
+        v-bind:key="status"
+        @click="handleButtonClick"
         size="large"
         color="linear-gradient(to right, #4bb0ff, #6149f6)"
         class="account-button"
       >
-        登录
+        {{buttonMessage}}
       </van-button>
-      <span class="account-forget">忘记密码?</span>
-      <span class="account-register">立即注册</span>
+
+      <transition name="van-fade">
+        <div v-if="status==='login'">
+          <span @click="setStatus('forget')" class="account-forget">忘记密码?</span>
+          <span @click="setStatus('register')" class="account-register">立即注册</span>
+        </div>
+      </transition>
     </van-col>
   </van-row>
 </template>
 
 <script>
 export default {
-  name: 'Account'
+  name: 'Account',
+  data () {
+    return {
+      status: 'login'
+    }
+  },
+  computed: {
+    buttonMessage: function () {
+      switch (this.status) {
+        case 'login':
+          return '登陆'
+        case 'register':
+          return '注册'
+        case 'forget':
+          return '重置密码'
+        default:
+          return '未知错误'
+      }
+    }
+  },
+  methods: {
+    setStatus: function (status) {
+      this.status = status
+    },
+    handleButtonClick: function () {
+      switch (this.status) {
+        case 'login':
+          // handle login
+          this.$store.commit('changeLoginStatus')
+          this.$router.back()
+          break
+        case 'register':
+          // handle register
+          this.status = 'login'
+          break
+        case 'forget':
+          // handle forget
+          this.status = 'login'
+          break
+        default:
+          break
+      }
+    }
+  }
 }
 </script>
 
 <style lang="less" scoped>
   .account {
-    &-title span{
+    &-title span {
       display: inline;
       margin-left: 16px;
       font-size: 15vw;
       vertical-align: top;
     }
+
     &-title img {
       width: 30vw;
     }
-    &-input{
+
+    &-span {
+      font-size: 8vw;
+      font-weight: bold;
+    }
+
+    &-input {
       margin-top: 5vh;
     }
-    &-button{
+
+    &-button {
       margin-top: 5vh;
     }
-    &-forget{
+
+    &-forget {
       float: left;
       margin-top: 5px;
     }
-    &-register{
+
+    &-register {
       float: right;
       margin-top: 5px;
     }

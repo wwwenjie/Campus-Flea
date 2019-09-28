6ac37857ca5453c26927692d2fb1984aacdf13cb
lKingl <jinwenjie@live.com>
2019-09-28 19:05:11 +0800

    feat(browse): add search, merge tabs

--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -1,18 +1,31 @@
 <template>
   <div>
+    <van-overlay
+      :show="show"
+      style="margin-top: 54px"
+      :z-index=2
+    />
     <form action="/">
       <van-search
-        v-model="searchValue"
+        v-model="searchitem"
         placeholder="请输入搜索关键词"
         shape="round"
-        @search="onSearch"></van-search>
+        @search="onSearch"
+        @focus="dropdownShow"
+        @blur="dropdownShow"
+        @input="searchLive"
+      >
+      </van-search>
     </form>
+    <van-cell-group v-if="dropdown" class="search-list">
+      <van-cell v-for="item of searchList" :key="item" :title="item"/>
+    </van-cell-group>
     <van-swipe :autoplay="3000">
       <van-swipe-item v-for="(image, index) in images" :key="index">
         <img v-lazy="image"/>
       </van-swipe-item>
     </van-swipe>
-    <van-tabs v-model="activeTab" sticky animated swipeable>
+    <van-tabs @change="tabsChange" sticky animated swipeable>
       <van-tab v-for="tab in tabs" :key="tab.name" :title="tab.title" :name="tab.name">
         <van-list
           v-model="loading"
@@ -23,18 +36,14 @@
         >
           <van-grid :gutter="10" :column-num="2" :border="false" class="grid">
             <van-grid-item
-              @click="detail(id)"
-              v-for="value in data"
-              :key="value.id">
-              <card :url="value.url" :name="value.name" :price="value.price"></card>
+              @click="goToDetail(item.id)"
+              v-for="item in data"
+              :key="item.id">
+              <card :thumb="item.thumb" :title="item.title" :price="item.price"></card>
             </van-grid-item>
           </van-grid>
         </van-list>
       </van-tab>
-      <van-tab title="求助" name="b">内容 2</van-tab>
-      <van-tab title="书本" name="c">内容 3</van-tab>
-      <van-tab title="服装" name="d">内容 3</van-tab>
-      <van-tab title="服装" name="e">内容 3</van-tab>
     </van-tabs>
     <tabs></tabs>
   </div>
@@ -49,115 +58,71 @@ export default {
   },
   data () {
     return {
+      show: false,
+      dropdown: false,
+      searchList: [],
       loading: false,
       finished: false,
-      tabs: [
-        {
-          title: '测试',
-          name: 'a'
-        },
-        {
-          title: '测试b',
-          name: 'b'
-        },
-        {
-          title: '测试c',
-          name: 'c'
-        }
-      ],
-      data: [
-        {
-          id: 1,
-          url: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: '免押金出租索尼E口24 1.4 GM大师定焦镜头全画幅 免押金出',
-          price: 1200
-        },
-        {
-          id: 2,
-          url: 'https://img.yzcdn.cn/vant/apple-1.jpg',
-          name: 'sss',
-          price: 50
-        },
-        {
-          id: 3,
-          url: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'ss',
-          price: 30
-        },
-        {
-          id: 4,
-          url: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'asdsadasd',
-          price: 10
-        },
-        {
-          id: 5,
-          url: 'https://img.yzcdn.cn/vant/apple-1.jpg',
-          name: 'sss',
-          price: 50
-        },
-        {
-          id: 6,
-          url: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'ss',
-          price: 30
-        },
-        {
-          id: 20,
-          url: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'asdsadasd',
-          price: 10
-        },
-        {
-          id: 12,
-          url: 'https://img.yzcdn.cn/vant/apple-1.jpg',
-          name: 'sss',
-          price: 50
-        },
-        {
-          id: 13,
-          url: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'ss',
-          price: 30
-        },
-        {
-          id: 7,
-          url: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'asdsadasd',
-          price: 10
-        },
-        {
-          id: 8,
-          url: 'https://img.yzcdn.cn/vant/apple-1.jpg',
-          name: 'sss',
-          price: 50
-        },
-        {
-          id: 9,
-          url: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'ss',
-          price: 30
-        },
-        {
-          id: 10,
-          url: 'https://www.w3schools.com/howto/img_avatar.png',
-          name: 'asdsadasd',
-          price: 10
-        }
-      ],
-      searchValue: null,
       images: [
         'https://img.yzcdn.cn/vant/apple-1.jpg',
         'https://img.yzcdn.cn/vant/apple-2.jpg',
         'https://img.yzcdn.cn/vant/apple-3.jpg',
         'https://img.yzcdn.cn/vant/apple-4.jpg'
       ],
-      activeTab: 'a'
+      tabs: [
+        {
+          title: '最新',
+          name: 'newest'
+        },
+        {
+          title: '书籍',
+          name: 'book'
+        },
+        {
+          title: '生活用品',
+          name: 'daily'
+        },
+        {
+          title: '3C数码',
+          name: '3c'
+        },
+        {
+          title: '鞋服美妆',
+          name: 'dress'
+        },
+        {
+          title: '求助',
+          name: 'help'
+        }
+      ],
+      data: [
+        {
+          id: 1,
+          thumb: 'https://img.yzcdn.cn/vant/apple-1.jpg',
+          title: '免押金出租索尼E口24 1.4 GM大师定焦镜头全画幅 免押金出',
+          price: 1200
+        },
+        {
+          id: 2,
+          thumb: 'https://img.yzcdn.cn/vant/apple-1.jpg',
+          title: 'sss',
+          price: 50
+        }
+      ],
+      searchitem: null
     }
   },
   methods: {
     onSearch: function () {
-
+      // onSearch
+    },
+    searchLive (value) {
+      this.searchList.push(value)
+    },
+    dropdownShow () {
+      this.show = !this.show
+      this.dropdown = !this.dropdown
+      this.searchList = []
     },
     onLoad () {
       // axios
@@ -165,12 +130,13 @@ export default {
       // if r = null
       this.finished = true
     },
-    detail (id = 1) {
-      this.$router.push({ name: 'detail', params: { id } })
-    }
-  },
-  mount: {
-    fisrtLoad () {
+    goToDetail (id = 1) {
+      this.$router.push({ name: 'detail', params: { id: id.toString() } })
+    },
+    tabsChange (name, title) {
+      // refresh data
+      console.log(name)
+      console.log(title)
     }
   }
 }
@@ -184,5 +150,11 @@ export default {
 
   .grid {
     margin-bottom: 100px;
+  }
+
+  .search-list {
+    z-index: 2;
+    position: fixed;
+    width: 100vw;
   }
 </style>

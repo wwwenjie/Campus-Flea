5191ee327e1b19b416865053c2d5c91cd0b1495e
King <jinwenjie@live.com>
2019-10-09 19:48:57 +0800

    fix(django/goods): add detail view which was removed by mistake

--- Django/apps/goods/urls.py
+++ Django/apps/goods/urls.py
@@ -3,7 +3,8 @@ from apps.goods.views import GoodsMainView, GoodsUploadView, GoodsDetailView, Go
 
 urlpatterns = [
     url(r'^browse$', GoodsMainView.as_view(), name='goodsBrowse'),
-    url(r'^search$', GoodsSearchView.as_view(), name='goodsDetail'),
+    url(r'^detail$', GoodsDetailView.as_view(), name='goodsDetail'),
+    url(r'^search$', GoodsSearchView.as_view(), name='goodsSearch'),
     url(r'^browse$', GoodsMainView.as_view(), name='goodsBrowse'),
     url(r'^upload$', GoodsUploadView.as_view(), name='goodsUpload')
 ]

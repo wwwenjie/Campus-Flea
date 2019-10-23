9727ea19546a50f9827b1d564d48d5e31ab4687e
lKingl <jinwenjie@live.com>
2019-10-23 22:55:21 +0800

    refactor(django/db): refactor database

--- Django/apps/goods/models.py
+++ Django/apps/goods/models.py
@@ -13,15 +13,15 @@ class Goods(BaseModel):
         (2, '待审核'),
     )
     status = models.IntegerField(default=0, choices=STATUS_CHOICE, verbose_name='商品状态')
-    title = models.CharField(max_length=40, default='', verbose_name='商品标题')
-    detail = models.TextField(default='', verbose_name='商品详情')
-    category = models.CharField(max_length=20, default='', verbose_name='商品类型')
-    url = models.TextField(default='', verbose_name='商品图片链接')
-    img_hash = models.TextField(default='', verbose_name='商品图片hash')
-    seller_id = models.IntegerField(default='', verbose_name='卖家id')
+    title = models.CharField(max_length=40, default='未获取到商品标题', verbose_name='商品标题')
+    detail = models.TextField(default='该商品未留下详情', verbose_name='商品详情')
+    category = models.CharField(max_length=20, default='其他', verbose_name='商品类型')
+    url = models.TextField(null=True, verbose_name='商品图片链接')
+    img_hash = models.TextField(null=True, verbose_name='商品图片hash')
+    seller_id = models.IntegerField(verbose_name='卖家id')
     price = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'), verbose_name='商品价格')
     express = models.DecimalField(max_digits=10, decimal_places=2, default=Decimal('0.00'), verbose_name='商品运费')
-    area = models.CharField(max_length=20, default='', verbose_name='发货地')
+    area = models.CharField(max_length=100, null=True, verbose_name='发货地')
 
     class Meta:
         db_table = 'SchoolFleasPro_goods'
--- Django/apps/user/models.py
+++ Django/apps/user/models.py
@@ -8,13 +8,19 @@ from db.base_model import BaseModel
 class User(BaseModel):
     """用户模型类"""
     username = models.CharField(max_length=20, verbose_name='用户名称')
-    email = models.CharField(max_length=20, verbose_name='邮件')
-    password = models.CharField(max_length=500, verbose_name='用户密码')
+    email = models.EmailField(null=True, verbose_name='邮件')
+    password = models.TextField(max_length=500, verbose_name='用户密码')
     last_login = models.DateTimeField(auto_now=True, verbose_name='最后登陆时间')
-    headimg = models.ImageField(upload_to='user', verbose_name='头像')
-    phone = models.CharField(max_length=20, verbose_name='电话')
-    address = models.CharField(max_length=200, verbose_name='收货地址')
-    user_token = models.CharField(max_length=500, verbose_name='用户token')
+    avatar = models.URLField(null=True, verbose_name='头像')
+    phone = models.CharField(max_length=20, null=True, verbose_name='电话')
+    address = models.CharField(max_length=200, null=True, verbose_name='收货地址')
+    user_token = models.TextField(max_length=500, null=True, verbose_name='用户token')
+    # 杂项
+    sex = models.CharField(max_length=5, null=True, verbose_name='性别')
+    bday = models.DateTimeField(blank=True, null=True, verbose_name='生日')
+    bio = models.CharField(max_length=50, null=True, verbose_name='简介')
+    qq = models.CharField(max_length=30, null=True, verbose_name='QQ')
+    wechat = models.CharField(max_length=30, null=True, verbose_name='微信')
 
     class Meta:
         db_table = 'SchoolFleasPro_user'
